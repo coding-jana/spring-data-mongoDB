cf98720a42212dbb2f0ff5b6c3154462723586f8
Jana Schaeffer <ja-schaeffer@web.de>
2024-06-13 17:07:10 +0200

    added Query by Criteria

--- src/main/java/com.spring.data.mongodb/Student.java
+++ src/main/java/com.spring.data.mongodb/Student.java
@@ -2,11 +2,11 @@ package com.spring.data.mongodb;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.util.List;
 
 @Data
@@ -16,8 +16,13 @@ public class Student {
     private String id;
     private String firstName;
     private String lastName;
+    @Indexed(unique = true)
     private String email;
     private Gender gender;
+    private Address address;
+    private List<String> favoriteSubjects;
+    private BigDecimal totalSpentInBooks;
+    private LocalDateTime created;
 
     public Student(String firstName, String lastName, String email, Gender gender, Address address, List<String> favoriteSubjects, BigDecimal totalSpentInBooks, LocalDateTime created) {
         this.firstName = firstName;
@@ -30,9 +35,5 @@ public class Student {
         this.created = created;
     }
 
-    private Address address;
-    private List<String> favoriteSubjects;
-    private BigDecimal totalSpentInBooks;
-    private LocalDateTime created;
 
 }
